0e1a1a45bb2e3036822b81628de01fbf117f4418
dk\ivd <ivd@sitecore.net>
2018-08-25 14:00:26 +0800

    Inject attributes to IndexedField

---
+++ src/Sitecore.Support.218625/ExtendedIndexedField.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Sitecore.Support
+{
+  public class ExtendedIndexedField : Sitecore.ContentSearch.Azure.Models.IndexedField
+  {
+    public ExtendedIndexedField(string name, string type, bool key, bool searchable) : base(name, type, key, searchable)
+    {
+    }
+
+    public ExtendedIndexedField(string name, string type, bool key, bool searchable, bool retrievable, bool sortable, bool facetable, bool filterable)
+      : base(name, type, key, searchable, retrievable)
+    {
+      this.Sortable = sortable;
+      this.Filterable = filterable;
+      this.Facetable = facetable;
+    }
+
+    public bool Sortable { get; }
+
+    public bool Facetable { get; }
+
+    public bool Filterable { get; }
+  }
+}
